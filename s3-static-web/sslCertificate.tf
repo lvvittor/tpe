a004769ba2750b277b35195450bb02db8fbe5e01
IanSzejer <75545451+IanSzejer@users.noreply.github.com>
2024-06-01 21:02:23 -0300

    Create sslCertificate.tf
    Created a tf with sslCertificate in the region to allow domain routing

---
+++ s3-static-web/sslCertificate.tf
@@ -0,0 +1,29 @@
+resource "aws_acm_certificate" "this" {
+  domain_name       = "itbamafia.com"
+  validation_method = "DNS"
+
+  tags = {
+    Name = "itbamafia.com"
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = var.route53_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
+
+resource "aws_acm_certificate_validation" "this" {
+  certificate_arn         = aws_acm_certificate.this.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
