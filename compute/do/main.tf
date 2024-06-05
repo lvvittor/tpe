5f7e7d75e92cb2831ff170180e0200dd13632d9c
Lucas Vittor <lvvittor@gmail.com>
2024-06-05 19:32:00 -0300

    chore: add firewall to do

--- compute/do/main.tf
+++ compute/do/main.tf
@@ -19,3 +19,49 @@ resource "digitalocean_droplet" "hello_world" {
     timeout     = "2m"
   }
 }
+
+resource "digitalocean_firewall" "web" {
+  name = "hello-world-only-allow-ssh-http-and-https"
+
+  droplet_ids = [digitalocean_droplet.hello_world.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
