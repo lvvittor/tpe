cece7f0ff78eafff932c4fe8dd3c8895631dee93
IanSzejer <75545451+IanSzejer@users.noreply.github.com>
2024-06-05 16:36:07 -0300

    Compute diagram file

---
+++ compute/diagram_compute.py
@@ -0,0 +1,9 @@
+from diagrams import Diagram, Cluster
+from diagrams.aws.compute import EC2,ECS
+
+with Diagram("Simple EC2 in region", show=False, direction="TB") as one:
+    with Cluster("Region: us-east-1"):
+      with Cluster("VPC 10.0.0.0/16"):
+        svc_group = [EC2("EC2_1")]
+
+one
